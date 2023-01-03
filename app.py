85d32b868ac41a1c2e97e99d7cda0e9a7892d787
Emile Greyling <emilegreyling14@gmail.com>
2023-01-03 14:09:23 +0200

    Modified files to be able to download mp3 format as well

--- app.py
+++ app.py
@@ -1,35 +1,30 @@
-from flask import Flask, render_template, request, redirect, send_file, url_for
+from flask import Flask, render_template, request, send_file
 from pytube import YouTube
 
-# Flask app
 app = Flask(__name__)
 
-
-# Main site
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def index():
-    return render_template('index.html', video="False")
+  return render_template('index.html')
 
-# Route to download the video
-@app.route('/download', methods=['POST'])
+@app.route('/download')
 def download():
-    # Get the URL of the video to download
-    video_id = request.form.get('video_id')
-    format = request.form.get('format')
-
-    if not format or not video_id:
-        return render_template('error.html', message = 'Please fill in all fields!')
-
+  # Get the URL of the YouTube video
+  url = request.args.get('url')
+  format = request.args.get('format')
+  
+  yt = YouTube(url)
+  if format == 'mp4':
     # Download the video
-    file_path = '/YouTube Downloads'
-    downloadVideoMp4(video_id, file_path)
-    
-    # Send the video to the browser as a response
-    return render_template('index.html', form_submitted=True, message=f'Your file was saved at {file_path}')
-
-
-def downloadVideoMp4(video_id, file_path):
-    # Get the video stream and return it
-    yt = YouTube(f'https://www.youtube.com/watch?v={video_id}')
     video = yt.streams.get_highest_resolution()
-    video = video.download(file_path)
+    file_path = video.download('/YouTube Downloads')
+  elif format == 'mp3':
+    mp3_stream = yt.streams.get_audio_only()
+    file_path = mp3_stream.download('/YouTube Downloads')
+
+  # Send the file to the browser
+  return send_file(file_path, as_attachment=True)
+
+@app.route('/error')
+def error():
+    return render_template('error.html', message='Please fill in all fields!')
--- static/index.js
+++ static/index.js
@@ -1,32 +1,45 @@
-// // DOM Elements
-// const video_id = document.querySelector('#url');
-// const format = document.querySelector('#format');
-// const form = document.querySelector('#download_form');
+// DOM Elements
+const url = document.querySelector('#url');
+const format = document.querySelector('#format');
+const title = document.querySelector('#title');
+const form = document.querySelector('#download_form');
 
-// // Listen for a click on the download button
-// downloadBtn.addEventListener('submit', downloadVideo())
+// Listen for a click on the download button
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    downloadVideo();
+});
 
-// function downloadVideo(e) {
-//     e.preventDefault();
+function downloadVideo() {
+    if (url.value == '' || format.value == '' || title.value == '') {
+        location.href = '/error';
+        return;
+    }
+    // Download the video
+    fetch(`/download?url=${url.value}&format=${format.value}`)
+        .then(response => response.blob())
+        .then(file => {
+        // Create a temporary anchor element
+        const a = document.createElement('a');
 
-//     // Download the video
-//     fetch(`/download?url=https://www.youtube.com/watch?v=${video_id}`)
-//         .then(response => {
-//         // Create a temporary anchor element
-//         const a = document.createElement('a');
-//         a.style.display = 'none';
-//         document.body.appendChild(a);
+        // Set the href and download attributes of the anchor element
+        let tempUrl = URL.createObjectURL(file);
+        a.href = tempUrl;
+        a.download = `${title.value}.${format.value}`;
 
-//         // Set the href and download attributes of the anchor element
-//         a.href = window.URL.createObjectURL(response.blob());
-//         a.download = 'video.mp4';
+        // Add the element to the DOM
+        // a.style.display = 'none';
+        document.body.appendChild(a);
 
-//         // Click the anchor element to trigger the download
-//         a.click();
+        // Click the anchor element to trigger the download
+        a.click();
 
-//         // Clean up
-//         window.URL.revokeObjectURL(a.href);
-//         document.body.removeChild(a);
-//         });
-// }
+        // Clean up
+        URL.revokeObjectURL(tempUrl);
+        a.remove()
+        url.value = '';
+        title.value = '';
+        format.value = '';
+    });
+}
 
